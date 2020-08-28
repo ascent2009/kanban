688056f23829b8eb2bd112a9cf3d557326a0cbab
ascent2009 <ascent2009@hotmail.com>
2020-08-28 20:22:29 +0300

    Fix: move Footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <h1>Awesome Kanban Board</h1>
-        <Header />
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Main />
-    </div>
-  );
+    this.state = {
+      tasks: 0,
+      finishedTasks: 0,
+    };
+  }
+
+  activeCounter = (value) => {
+    this.setState({
+      tasks: value,
+    });
+  };
+
+  minusActiveCounter = (value) => {
+    this.setState({
+      tasks: value,
+    });
+  };
+
+  finishedCounter = (value) => {
+    this.setState({
+      finishedTasks: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1>Awesome Kanban Board</h1>
+          <Header />
+        </header>
+
+        <Main
+          active={this.activeCounter}
+          minusActive={this.minusActiveCounter}
+          finished={this.finishedCounter}
+        />
+
+        <Footer
+          activeTasks={this.state.tasks}
+          finishedTasks={this.state.finishedTasks}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,51 +1,45 @@
 import React from "react";
 import Backlog from "./Backlog/Backlog";
-import Footer from "./Footer/Footer";
+// import Footer from "./Footer/Footer";
 import "./Main.css";
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      tasks: 0,
-      finishedTasks: 0,
-    };
-  }
+  //   this.state = {
+  //     tasks: 0,
+  //     finishedTasks: 0,
+  //   };
+  // }
 
-  activeCounter = (value) => {
-    this.setState({
-      tasks: value,
-    });
-  };
+  // activeCounter = (value) => {
+  //   this.setState({
+  //     tasks: value,
+  //   });
+  // };
 
-  minusActiveCounter = (value) => {
-    this.setState({
-      tasks: value,
-    });
-  };
+  // minusActiveCounter = (value) => {
+  //   this.setState({
+  //     tasks: value,
+  //   });
+  // };
 
-  finishedCounter = (value) => {
-    this.setState({
-      finishedTasks: value,
-    });
-  };
+  // finishedCounter = (value) => {
+  //   this.setState({
+  //     finishedTasks: value,
+  //   });
+  // };
 
   render() {
     return (
-      <>
-        <div>
-          <Backlog
-            active={this.activeCounter}
-            minusActive={this.minusActiveCounter}
-            finished={this.finishedCounter}
-          />
-        </div>
-        <Footer
-          activeTasks={this.state.tasks}
-          finishedTasks={this.state.finishedTasks}
+      <div>
+        <Backlog
+          active={this.props.active}
+          minusActive={this.props.minusActive}
+          finished={this.props.finished}
         />
-      </>
+      </div>
     );
   }
 }
